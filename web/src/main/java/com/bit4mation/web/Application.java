fc39603cc242d2c793c29cdcbe2fc60d9f2a38c8
Mikolaj Sobkowiak <mikolaj.sobkowiak@contractors.roche.com>
2016-09-12 14:40:01 +0200

    start-class after refactor fix

--- web/src/main/java/com/bit4mation/web/Application.java
+++ web/src/main/java/com/bit4mation/web/Application.java
@@ -3,16 +3,17 @@ package com.bit4mation.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Spring boot configuration class
  */
+@Configuration
 @Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.bit4mation"})
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
